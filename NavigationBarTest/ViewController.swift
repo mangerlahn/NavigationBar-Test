bed9d28664a751341f16cffd83e4f35cd8cb98c3
Max Langer <maxlanger@me.com>
2015-12-28 11:55:56 +0100

    Initial Commit

---
+++ NavigationBarTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationBarTest
+//
+//  Created by Max Langer on 28.12.15.
+//  Copyright © 2015 Max Langer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
